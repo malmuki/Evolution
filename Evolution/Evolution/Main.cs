c396941a9c84bdd49c3420c23cf8d8433e286557
Francois Chantal <geniousmouton@gmail.com>
2016-01-19 10:13:56 -0400

    puch it m'am

---
+++ Evolution/Evolution/Main.cs
@@ -0,0 +1,15 @@
+﻿using Evolution.game;
+
+namespace Evolution
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            game.InitBoard();
+            game.StartGame();
+        }
+    }
+}

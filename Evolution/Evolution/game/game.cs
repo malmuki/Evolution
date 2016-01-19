bf260161643de3f05fa55c4a47e7b6b403a01537
Olivier Precourt <olivierprecourt@live.ca>
2016-01-19 10:42:38 -0500

    tweak count neighbors

--- Evolution/Evolution/game/game.cs
+++ Evolution/Evolution/game/game.cs
@@ -2,9 +2,24 @@
 {
     public class Game
     {
-        private const int MaxTurn = 500;
+        private const int MaxTurn = 400;
         private const int boardSize = 500;
         private bool[,] _array = new bool[boardSize, boardSize];
+
+        private Pos[] neighborsNavigator;
+
+        private void initNeighborsNavigator()
+        {
+            neighborsNavigator = new Pos[] {new Pos{X = -1, Y = -1}, 
+                                            new Pos{X = -1, Y = 0}, 
+                                            new Pos{X = -1, Y = 1}, 
+                                            new Pos{X = 0, Y = -1}, 
+                                            new Pos{X = 0, Y = 1}, 
+                                            new Pos{X = 1, Y = -1}, 
+                                            new Pos{X = 1, Y = 0}, 
+                                            new Pos{X = 1, Y = 1}, 
+                                           };
+        }
 
         public void ReinitBoard()
         {
@@ -19,6 +34,8 @@
 
         public void InitBoard()
         {
+            initNeighborsNavigator();
+
             for (int i = 0; i < boardSize - 1; i++)
             {
                 for (int j = 0; j < boardSize - 1; j++)
@@ -67,24 +84,38 @@
             return averageLivingCell;
         }
 
+        //private int GetNbNeighbors(int x, int y)
+        //{
+        //    int nbNeighbors = 0;
+        //    for (int i = x - 1; i < x + 1; i++)
+        //    {
+        //        if (x != 0 && x == boardSize - 1)
+        //        {
+        //            for (int j = y - 1; j < y + 1; j++)
+        //            {
+        //                if (y != 0 && y == boardSize - 1)
+        //                {
+        //                    if (_array[i, j])
+        //                        nbNeighbors++;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return nbNeighbors;
+        //}
+
         private int GetNbNeighbors(int x, int y)
         {
-            int nbNeighbors = 0;
-            for (int i = x - 1; i < x + 1; i++)
-            {
-                if (x != 0 && x == boardSize - 1)
+           int nbNeighbors = 0;
+           
+            foreach(Pos pos in neighborsNavigator){
+                if ((x + pos.X >= 0 && x + pos.X <= boardSize - 1) && (y + pos.Y <= boardSize - 1 && y + pos.Y >= 0) && (_array[x + pos.X, y + pos.Y]))
                 {
-                    for (int j = y - 1; j < y + 1; j++)
-                    {
-                        if (y != 0 && y == boardSize - 1)
-                        {
-                            if (_array[i, j])
-                                nbNeighbors++;
-                        }
-                    }
+                        ++nbNeighbors;
                 }
             }
-            return nbNeighbors;
+
+           return nbNeighbors;
         }
 
         public void SetLiving(Pos[] startBoard)

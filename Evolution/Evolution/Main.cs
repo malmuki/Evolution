1e905d56a342ecbcc7aa97d8b150d4b40f763e15
Olivier Precourt <olivierprecourt@live.ca>
2016-01-21 11:22:24 -0500

    affichage console

--- Evolution/Evolution/Main.cs
+++ Evolution/Evolution/Main.cs
@@ -8,17 +8,13 @@ namespace Evolution
         public static void Main(string[] args)
         {
             Game game = new Game();
-            Visual ui = new Visual();
 
-            game.InitBoard(ui);
+            game.InitBoard();
             game.SetLiving(new Pos[]
             {
                 new Pos{X = 1,Y=0}, new Pos(){X = 1,Y=1}, new Pos(){X = 1,Y=2}
             });
-
-            Thread oThread = new Thread(new ThreadStart(game.StartGame));
-
-            oThread.Start();
+            game.StartGame();
         }
     }
 }
